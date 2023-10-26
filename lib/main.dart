f71647dfd842879317537c8a4cbf8a400cdd24b3
Mohamed01Morsy01Hasballah <moh.morsy2211@gmail.com>
2023-10-26 23:23:24 +0200

    problem

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
-//jjjj
+//jjjjkkk
       DioHelper.init();
       await CacheHelper.getinit();
      bool? onboard= CacheHelper.getBooleanData(key: 'onBoarding');
